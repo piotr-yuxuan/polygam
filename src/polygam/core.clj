a4152f0d95d5074f6ee3af3bcb963c4ea769055b
piotr-yuxuan <pierre2b@adikteev.com>
2016-09-14 20:28:25 +0200

    Strengthen availableo with another example

--- src/polygam/core.clj
+++ src/polygam/core.clj
@@ -8,82 +8,37 @@
 
 (declare yap yuk)
 
-(comment
-  "Other settings"
-  "This a compilation of very vicious situations."
-  "A propoer way to deal with them would be to restrict the graph we are working
-  on: instead of a tree (directed acyclic graph) we could choose a directed
-  acyclic graph which has at a root (a node all other veritces are reachable
-  from) and whose underlying undirected graph is still acyclic."
-  "Perhaps I could generalise little by little"
-  ""
-  "Anyway, for now the goal `availableo` in this file has been demonstrated
-  correct for one graph. How could I prove it more formally?"
-  (def vertices
-    [:a :b :c :d :e :f :g :h :i :j
-     :k :l :m :n :o :p :q :r :s :t
-     :u :v :w :x :y :z :a° :b° :c°
-     :d° :e° :f° :g° :h° :i° :j° :k°
-     :l° :m° :n :o°])
+(def vertices
+  [:a :b :c :d :e :f :g :h :i :j
+   :l :m :n :o :p :q :r :s :t
+   :k :kk
+   :u :v :w :x :y :z :a° :b° :p°])
 
-  (def kinship
-    {:a [:b :c :d]
-     :c [:k :l]
-     :l [:m :n :o]
-     :m [:r :s]
-     :n [:q]
-     :o [:p]
-     :b [:e :f :g]
-     :g [:j]
-     :f [:h :i]
-     :d [:t :u :v]
-     :v [:w :x]
-     :x [:y]
-     :y [:z :a° :b°]
-     :l° [:b° :m° :n° :o°]
-     :j° [:l°]
-     :h° [:j° :k°]
-     :e° [:h° :i°]
-     :c° [:d° :e° :f°]
-     :d° [:g° :v]})
+(def kinship
+  {:a [:b :i :t]
+   :b [:c :d :g]
+   :d [:e :f]
+   :g [:h]
+   :i [:j :l]
+   :l [:m :p :r]
+   :m [:n :o]
+   :p [:q]
+   :r [:s]
+   :t [:u :v :w]
+   :w [:x :y]
+   :y [:z]
+   :z [:a° :b° :p°]})
 
-  (def favour
-    (db
-     [yap :a]
-     [yap :m]
-     [yap :f]
-     [yap :h°]
-     [yap :o°]
-     [yap :y]
+(def favour
+  (db
+   [yap :a]
+   [yap :d]
+   [yap :m]
+   [yap :z]
+   [yap :k]
 
-     [yuk :n]
-     [yuk :v]
-     [yuk :l°])))
-
-(comment
-  (def vertices
-    [:a :b :c :d :e :f :g :h :i :j :k :l :m :n :o :p :q])
-
-  (def kinship
-    {:a [:b :m :c]
-     :b [:d :e :f]
-     :c [:h]
-     :e [:r]
-     :h [:i :j :k :l]
-     :m [:n :o]
-     :n [:g :q]
-     :q [:p]})
-
-  (def favour
-    (db
-     [yap :a]
-     [yap :c]
-     [yap :j]
-     [yap :k]
-     [yap :n]
-
-     [yuk :b]
-     [yuk :h])))
+   [yuk :p]
+   [yuk :w]))
 
 (db-rel vertex
  ^:index x)
@@ -121,25 +76,42 @@
   y a descandant of x."
   (partial order-relationo child))
 
+(defn unstrict-order-relationo
+  "Abstract the general pattern of a order relation in a logic, relational way."
+  [relation x y]
+  (conde
+   [(l/== x y)]
+   [(relation x y)]
+   [(fresh [z]
+      (relation x z)
+      (order-relationo relation z y))]))
+
+(def light-kino
+  "A goal where the two inputs x and y share kinship: x is an ancestor of y and
+  y a descandant of x."
+  (partial order-relationo child))
+
 (defn tope
   "no parents"
   [x]
   (nafc #(fresh [u] (child u %)) x))
 
-(with-dbs [definitions kin]
-  (run* [q]
-    (vertex q)
-    (kino q :f)))
+(comment
+  (with-dbs [definitions kin]
+    (run* [q]
+      (vertex q)
+      (kino q :f))))
 
 (defn leafo
   "no descandants"
   [x]
   (nafc #(fresh [u] (child % u)) x))
 
-(with-dbs [definitions kin]
-  (run* [q]
-    (vertex q)
-    (leafo q)))
+(comment
+  (with-dbs [definitions kin]
+    (sort (run* [q]
+            (vertex q)
+            (leafo q)))))
 
 (defn boundaryo
   "Extrema of the order relation child"
@@ -147,10 +119,11 @@
   (conde [(leafo x)]
          [(tope x)]))
 
-(with-dbs [definitions kin]
-  (run* [q]
-    (vertex q)
-    (boundaryo q)))
+(comment
+  (with-dbs [definitions kin]
+    (run* [q]
+      (vertex q)
+      (boundaryo q))))
 
 (defn siblingso
   [x s]
@@ -159,17 +132,19 @@
     (child z s)
     (l/!= x s)))
 
-(with-dbs [definitions kin]
-  (run* [q]
-    (siblingso :l q)))
+(comment
+  (with-dbs [definitions kin]
+    (run* [q]
+      (siblingso :l q))))
 
-(with-dbs [definitions favour kin]
-  (run* [q]
-    (vertex q)
-    (yap q)
-    (siblingso :l q)))
+(comment
+  (with-dbs [definitions favour kin]
+    (run* [q]
+      (vertex q)
+      (yap q)
+      (siblingso :l q))))
 
-(defn impeachedo
+(defn empeachedo
   [q]
   (all
    (fresh [z]
@@ -184,16 +159,17 @@
   [q]
   (all
     (vertex q)
-    (nafc impeachedo q)
+    (nafc empeachedo q)
     (fresh [p]
            (conde [(tope q)]
              [(kino p q)])
-           (nafc impeachedo p))))
+           (nafc empeachedo p))))
 
-(with-dbs [definitions favour kin]
-  (distinct
-   (run* [q]
-         (yap-treeo q))))
+(comment
+  (with-dbs [definitions favour kin]
+    (distinct
+     (run* [q]
+       (yap-treeo q)))))
 
 (defn yuk-treeo
   [q]
@@ -203,9 +179,10 @@
             (kino p q)
             (yuk p))]))
 
-(with-dbs [definitions favour kin]
-  (run* [q]
-    (yuk-treeo q)))
+(comment
+  (with-dbs [definitions favour kin]
+    (run* [q]
+      (yuk-treeo q))))
 
 (defn son-of-yap-son-of-yuko [q]
   (fresh [a b]
@@ -218,16 +195,68 @@
     (l/!= a b)
     (l/!= b q)))
 
-(with-dbs [definitions favour kin]
-  (->
-   (run* [q]
-     (vertex q)
-     (nafc yuk q)
-     (conda [(tope q) (yap q)]
-            [(yap q)]
-            [(nafc impeachedo q)]
-            [(son-of-yap-son-of-yuko q)]))
-   sort))
+(defn empeached-treeo
+  [q]
+  (fresh [a]
+    (kino a q)
+    (empeachedo a)
+    (nafc yuk a)))
+
+(comment
+  (with-dbs [definitions favour kin]
+    (->
+     (run* [q]
+       (empeached-treeo q))
+     sort)))
+
+(defn son-of-empeachedo
+  [q]
+  (fresh [a b]
+    (kino a q)
+    (kino a b)
+    (empeachedo a)
+    (kino b q)
+    (nafc yuk a)
+    (yap b)))
+
+(comment
+  (with-dbs [definitions favour kin]
+    (->
+     (run* [q]
+       (son-of-empeachedo q))
+     sort)))
+
+(comment
+  (with-dbs [definitions favour kin]
+    (->
+     (run* [q]
+       (fresh [a b]
+         (kino a q)
+         (kino a b)
+         (empeachedo a)
+         (kino b q)
+         (nafc yuk a)
+         (yap b)))
+     sort)))
+
+(defn empeached-tree-prunedo
+  [q]
+  (fresh [a]
+    (kino a q)
+    (empeachedo a)
+    (nafc yuk a)))
+
+(comment
+  (with-dbs [definitions favour kin]
+    (->
+     (run* [q]
+       (vertex q)
+       (nafc yuk q)
+       (conda [(tope q) (yap q)]
+              [(yap q)]
+              [(nafc empeachedo q)]
+              [(son-of-yap-son-of-yuko q)]))
+     sort)))
 
 (defn availableo
   [q]
@@ -236,24 +265,36 @@
     (nafc yuk q)
     (conda [(tope q) (yap q)]
            [(yap q)]
-           [(nafc impeachedo q) (nafc yuk-treeo q)]
-           [(impeachedo q) fail]
            [(son-of-yap-son-of-yuko q)]
+           [(empeached-treeo q) (son-of-empeachedo q)]
+           [(nafc empeachedo q) (nafc yuk-treeo q)]
+           [(empeachedo q) fail]
            [(nafc yuk-treeo q)])))
 
-(with-dbs [definitions favour kin]
-  (sort
-   (time (run* [q]
-           (availableo q)))))
+(comment
+  (with-dbs [definitions favour kin]
+    (sort
+     (time (run* [q]
+             (availableo q))))))
 
-(with-dbs [definitions favour kin]
-  (sort
-   (run* [q]
-     (vertex q)
-     (nafc yuk-treeo q))))
+(-> (run* [q]
+      (availableo q))
+    sort
+    (conj :lol)
+    time)
 
-(with-dbs [definitions favour kin]
-  (sort
-   (time (run* [q]
-           (vertex q)
-           (availableo q)))))
+(comment
+  (with-dbs [definitions favour kin]
+    (sort
+     (run* [q]
+       (vertex q)
+       (son-of-yap-son-of-yuko q)))))
+
+(comment
+  (with-dbs [definitions favour kin]
+    (sort
+     (time (run* [q]
+             (vertex q)
+             ;;(availableo q)
+             (son-of-yap-son-of-yuko q)
+             )))))
