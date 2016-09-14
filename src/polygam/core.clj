8bf84629fc5eeac63e4ae8486454ebe485004073
piotr-yuxuan <pierre2b@adikteev.com>
2016-09-14 22:34:32 +0200

    Add relationnal goals
    Relational goals aren't well described yet.

--- src/polygam/core.clj
+++ src/polygam/core.clj
@@ -19,6 +19,8 @@
    :b [:c :d :g]
    :d [:e :f]
    :g [:h]
+   :h [:k]
+   :k [:kk]
    :i [:j :l]
    :l [:m :p :r]
    :m [:n :o]
@@ -263,8 +265,7 @@
   (all
     (vertex q)
     (nafc yuk q)
-    (conda [(tope q) (yap q)]
-           [(yap q)]
+    (conda [(yap q)]
            [(son-of-yap-son-of-yuko q)]
            [(empeached-treeo q) (son-of-empeachedo q)]
            [(nafc empeachedo q) (nafc yuk-treeo q)]
@@ -280,7 +281,6 @@
 (-> (run* [q]
       (availableo q))
     sort
-    (conj :lol)
     time)
 
 (comment
@@ -298,3 +298,107 @@
              ;;(availableo q)
              (son-of-yap-son-of-yuko q)
              )))))
+
+(comment
+  "Several possible explicit configurations:"
+  "C1 If you're yap, you are available"
+  "C2 If you're yuk, you're not available"
+  "C3 If you are the sibling of a yapped node without being yapped yourself, you
+  are not available: you are empeached"
+  "C4 If amongst your parents can be found both yap yuk and empeached nodes,
+  you're available if and only if: 1) every empeached parent is the parent of a
+  yap node and 2) every yuk parent is the parent of a yap node"
+  "C5 If amongst your parents can be found yap nodes and can be found neither
+  yuk nor empeached nodes, you're available.")
+
+(with-dbs [definitions favour kin]
+  (-> (run* [q]
+        (vertex q)
+        (fresh [z c]
+          (child z q)
+          (child z c)
+          (yap c)
+          (l/!= q c)))
+      time
+      distinct
+      sort))
+
+(defn c5o
+  [q]
+  (all
+   (nafc yap q)
+   (nafc yuk q)
+   (nafc empeachedo q)
+   (nafc
+    #(fresh [a]
+       (vertex a)
+       (kino a q)
+       (conde [(empeachedo a)]
+              [(yuk a)])))))
+
+(with-dbs [definitions favour kin]
+  (-> (run* [q]
+        (vertex q)
+        (c5o q))
+      time
+      distinct
+      sort))
+
+(with-dbs [definitions favour kin]
+  (-> (run* [q]
+        (vertex q)
+        (conde [(c1o q)]
+               [(c2o q) fail]
+               [(c3o q) fail]
+               [(c4o q)]
+               [(c5o q)]))
+      time
+      sort))
+
+(defn c4o
+  [q]
+  (fresh [a b]
+    (kino a q)
+    (kino b q)
+    (conde [(yuk b)]
+           [(fresh [z c]
+              (child z b)
+              (child z c)
+              (yap c)
+              (nafc yap b)
+              (nafc yuk b)
+              (l/!= q c))])
+    (fresh [c]
+      (yap c)
+      (kino c q)
+      (kino b c))))
+
+(with-dbs [definitions favour kin]
+  (-> (run* [q]
+        (vertex q)
+        (conde [(yap q)]
+               [(yuk q) fail]
+               [(fresh [z c]
+                  (child z q)
+                  (child z c)
+                  (l/!= q c)) fail]))
+    time
+    sort))
+
+(defn c3o
+  [q]
+  (fresh [z c]
+    (child z q)
+    (child z c)
+    (yap c)
+    (nafc yap q)
+    (nafc yuk q)
+    (l/!= q c)))
+
+(defn c1o
+  [q]
+  (yap q))
+
+(defn c2o
+  [q]
+  (yuk q))
